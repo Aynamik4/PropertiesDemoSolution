dbcf15c4fafdb635455489e1718f4368f14f11fc
Aynamik4 <violintailgut@gmail.com>
2020-11-02 21:38:57 +0100

    Added some initial code

---
+++ PropertiesDemoProject/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropertiesDemoProject
+{
+    class Person
+    {
+        //// .NET ver 1.
+        //private string name;
+
+        //public string Name
+        //{
+        //    get { return name; }
+        //    set { name = value; }
+        //}
+
+        //// An "automatic property" is transformed by the compiler to a full property.
+        //public string Name { get; set; }
+
+        //// Private set:er with full property.
+        //private string name;
+
+        //public string Name
+        //{
+        //    get { return name; }
+        //    private set { name = value; }
+        //}
+
+        //public Person(string aName)
+        //{
+        //    Name = aName;
+        //}
+
+        // Private set:er with automatic property.
+        public string Name { get; private set; }
+
+        public Person(string aName)
+        {
+            Name = aName;
+        }
+
+    }
+}
--- PropertiesDemoProject/Program.cs
+++ PropertiesDemoProject/Program.cs
@@ -6,6 +6,18 @@ namespace PropertiesDemoProject
     {
         static void Main(string[] args)
         {
+            //// 1.
+            //Person aPerson = new Person();
+            //aPerson.Name = "Håkan"; // Debug using "Step over properties..." unchecked.
+            //string s = aPerson.Name;
+
+            //// 2.
+            //Person aPerson = new Person("Håkan");
+            //string s = aPerson.Name;
+
+            // 3.
+            Person aPerson = new Person("Håkan");
+            string s = aPerson.Name;
         }
     }
 }
